edb8ae55961e9c57308a8dc97c95e778374a9c7e
RayLu11 <38595010+RayLu11@users.noreply.github.com>
2019-10-19 16:02:10 -0500

    add

---
+++ YunshiLu/Assets/Script/textWarning.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class textWarning : MonoBehaviour
+{
+    public Text textManage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textManage.enabled = false;
+    }
+
+    public void whenClick(Text textManage)
+    {
+        textManage.enabled = true;
+    }
+}
